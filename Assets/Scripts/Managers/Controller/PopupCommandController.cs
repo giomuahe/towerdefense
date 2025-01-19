ac53cb811d0bdfdc594fac9ac432469483ed5324
cavoisatthu <thanhbd92@gmail.com>
2025-01-20 00:24:17 +0700

    update UI 2 : thêm popup

---
+++ Assets/Scripts/Managers/Controller/PopupCommandController.cs
@@ -0,0 +1,74 @@
+﻿using Assets.Scripts.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupCommandController : MonoBehaviour
+{
+    public Text Tittle;
+    public Text Message;
+    public Button AcceptButton;
+    public Button CancelButton;
+
+    private EMESSAGETYPE messType;
+    private Action onAcceptBtnPressed;
+    private Action onCancelBtnPressed;
+
+    private void OnEnable()
+    {
+        AcceptButton.onClick.AddListener(OnAcceptButtonClick);
+        CancelButton.onClick.AddListener(OnCancelButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        AcceptButton.onClick.RemoveAllListeners();
+        CancelButton.onClick.RemoveAllListeners();
+    }
+
+    private void OnAcceptButtonClick()
+    {
+        // Gọi callback khi người dùng nhấn OK
+        onAcceptBtnPressed?.Invoke();
+        //Ẩn popup hiện tại
+        this.gameObject.SetActive(false);
+    }
+
+    private void OnCancelButtonClick()
+    {
+        // Gọi callback khi người dùng nhấn Cancel
+        onCancelBtnPressed?.Invoke();
+        //Ẩn popup hiện tại
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnCloseButtonClick()
+    {
+        OnCancelButtonClick();
+    }
+
+    public void Show(string message, string tittleCustom = "", Action callbackOk = null, Action CallbackCancel = null)
+    {
+        string tittle = "";
+        if (!string.IsNullOrEmpty(tittleCustom) && tittleCustom.Length <= 15)
+        {
+            tittle = tittleCustom;
+        }
+        else
+        {
+            tittle = "THÔNG BÁO";
+        }
+
+        onAcceptBtnPressed = callbackOk;
+        onCancelBtnPressed = CallbackCancel;
+
+        Tittle.text = tittle;
+        Message.text = message;
+
+        this.gameObject.SetActive(true);
+    }
+
+}
--- Assets/Scripts/Managers/Controller/PopupController.cs
+++ Assets/Scripts/Managers/Controller/PopupController.cs
@@ -1,11 +1,27 @@
+using Assets.Scripts.Enums;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PopupController : MonoBehaviour
 {
-    public void ShowConfirmPopup()
-    {
+    public PopupConfirmController confirmController;
+    public PopupCommandController commandController;
 
+    public void ShowConfirmPopup(EMESSAGETYPE msgType, string message, Action callback = null)
+    {
+        if (confirmController)
+            confirmController.Show(msgType, message, callback);
+        else
+            Debug.LogError("CANT_SHOW_CONFIRM_POPUP");
+    }
+
+    public void ShowCommandPopup(string message, string tittleCustom = "", Action callbackOk = null, Action CallbackCancel = null)
+    {
+        if (commandController)
+            commandController.Show(message, tittleCustom, callbackOk, CallbackCancel);
+        else
+            Debug.LogError("CANT_SHOW_COMMAND_POPUP");
     }
 }
