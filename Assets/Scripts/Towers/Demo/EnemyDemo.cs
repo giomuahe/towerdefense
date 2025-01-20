76f2f6e6a964ad96329259f418197c5395e6c627
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2025-01-20 09:42:03 +0700

    xử lý turet bắn đơn

--- Assets/Scripts/Towers/Demo/EnemyDemo.cs
+++ Assets/Scripts/Towers/Demo/EnemyDemo.cs
@@ -7,16 +7,18 @@ public class EnemyDemo : MonoBehaviour
     // Start is called before the first frame update
     public float maxHp;
     public float curHp;
+    public Transform axis;
     void Start()
     {
-        maxHp=300;
+        maxHp=1000;
         curHp=maxHp;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position+=new Vector3(0,0,0) * Time.deltaTime;
+        
+         transform.RotateAround(axis.position, Vector3.up, 20f * Time.deltaTime);
         
     }
     public void TakeDamage(float damage){
