fc7f79f1c7161cd02b74da18d16f4c0c8f3eb145
cavoisatthu <thanhbd92@gmail.com>
2025-01-20 21:09:40 +0700

    update

--- Assets/Scripts/Managers/BattleLogicManager.cs
+++ Assets/Scripts/Managers/BattleLogicManager.cs
@@ -15,7 +15,7 @@ namespace Assets.Scripts.Managers
     /// <summary>
     /// Quản lý luồng logic khi vào scene "MAP"
     /// </summary>
-    public class BattleLogicManager
+    public class BattleLogicManager: MonoBehaviour
     {
         //Core variable
         private string mapname = "";
