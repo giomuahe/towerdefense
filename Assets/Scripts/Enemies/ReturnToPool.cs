c36a221ddbabe9760d2c1f2487c6573e647c23ee
cavoisatthu <thanhbd92@gmail.com>
2025-01-21 10:48:19 +0700

    update để build

--- Assets/Scripts/Enemies/ReturnToPool.cs
+++ Assets/Scripts/Enemies/ReturnToPool.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
-using UnityEngine.Rendering.VirtualTexturing;
-
 public class ReturnToPool : MonoBehaviour
 {
     public ObjectPool<GameObject> Pool;
